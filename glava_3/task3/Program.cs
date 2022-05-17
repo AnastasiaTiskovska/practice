f9d232cecfaeb99eca54401a3b89527110c334ed
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-05-17 21:56:50 +0300

    glava3

---
+++ glava_3/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m1 = new DoubleMatr(3, 4);
+            m1.Fill();
+            m1.Info();
+            Console.WriteLine("=============================================================");
+            var m2 = new DoubleMatr(3, 4);
+            m2.Fill();
+            m2.Info();
+            Console.WriteLine("=============================================================");
+            var m3 = m1 + m2;
+            m3.Info();
+            Console.WriteLine("=============================================================");
+            var m5 = m1 - m2;
+            m5.Info();
+            Console.WriteLine("=============================================================");
+            var m4 = m1 + 100;
+            m4.Info();
+            Console.WriteLine("=============================================================");
+            Console.WriteLine($"сравнение м1 и м2 {m1==m2}");
+            Console.WriteLine($"доступ к индексу 2,3 матрицы м1  {m1[1,2]:F2}");
+        }
+    }
+}
---
+++ glava_3/task3/doublematr.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace task3
+{
+    class DoubleMatr
+    {
+        Random rand = new Random();
+
+        private double[,] _matr;
+        public int RowCount => _matr.GetLength(0);
+        public int ColumnCount => _matr.GetLength(1);
+
+        public double this[int i, int j] // индексатор
+        {
+            get
+            {
+                return _matr[i, j];
+            }
+        }
+        public DoubleMatr(double[,] matr)
+        {
+            _matr = matr;
+        }
+        public DoubleMatr(int rowCount, int columnCount)
+        {
+            _matr = new double[rowCount, columnCount];
+        }
+
+        public static DoubleMatr operator +(DoubleMatr currentMatr, DoubleMatr addMatr) // оператор + матр
+        {
+            double[,] result = new double[currentMatr.RowCount, currentMatr.ColumnCount];
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    result[i, j] = currentMatr[i, j] + addMatr[i, j];
+                }
+            }
+            return new DoubleMatr(result);
+        }
+        public static DoubleMatr operator -(DoubleMatr currentMatr, DoubleMatr substrMatr) // оператор - матр
+        {
+            double[,] result = new double[currentMatr.RowCount, currentMatr.ColumnCount];
+
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    result[i, j] = currentMatr[i, j] - substrMatr[i, j];
+                }
+            }
+            return new DoubleMatr(result);
+        }
+        public static DoubleMatr operator -(DoubleMatr currentMatr, int substrInt) // оператор - число
+        {
+            double[,] result = new double[currentMatr.RowCount, currentMatr.ColumnCount];
+
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    result[i, j] = currentMatr[i, j] - substrInt;
+                }
+            }
+            return new DoubleMatr(result);
+        }
+        public static DoubleMatr operator + (DoubleMatr currentMatr, int addInt) // оператор + число
+        {
+            double[,] result = new double[currentMatr.RowCount, currentMatr.ColumnCount];
+
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    result[i, j] = currentMatr[i, j] + addInt;
+                }
+            }
+            return new DoubleMatr(result);
+        }
+        public static bool operator ==(DoubleMatr currentMatr, DoubleMatr addMatr) // оператор ==
+        {
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    if (currentMatr[i,j]!=addMatr[i,j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static bool operator !=(DoubleMatr currentMatr, DoubleMatr addMatr) // оператор - матр
+
+        {
+            for (int i = 0; i < currentMatr.RowCount; i++)
+            {
+                for (int j = 0; j < currentMatr.ColumnCount; j++)
+                {
+                    if (currentMatr[i, j] != addMatr[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public void Info()
+        {
+            for (int i = 0; i < _matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matr.GetLength(1); j++)
+                {
+                    Console.Write($"{_matr[i, j],-7 :F2}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Fill()
+        {
+            for (int i = 0; i < _matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matr.GetLength(1); j++)
+                {
+                    _matr[i, j] = rand.NextDouble();
+                }
+            }
+        }
+
+    }
+}
+
