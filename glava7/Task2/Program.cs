c4f96952af4a113c5b21166c88c62337d681a718
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-05-20 11:29:42 +0300

    glava7

---
+++ glava7/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            Regex regex = new Regex(@"\b(\S+)\b(?=.*\b\1\b.*)");
+            MatchCollection matches = regex.Matches(str);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.Write(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+        }
+    }
+}
---
+++ glava7/Tast1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Tast1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, @"\S+[уеаоэяюи]+[уеаоэяюи]\b");
+            Console.WriteLine("Слова с 2 гласными в конце: ");
+            for (int i = matches.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(matches[i].ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
