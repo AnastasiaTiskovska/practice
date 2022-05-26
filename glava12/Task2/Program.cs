c39ee579d5049f7ef99db7863d35010d8c46cfc5
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-05-26 12:56:51 +0300

    ku-ku

---
+++ glava12/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выберите операцию: \n1. Сложение \n2. Вычитание \n3. Умножение \n4. Деление");
+            int operation = int.Parse(Console.ReadLine());
+            switch(operation)
+            {
+                case 1:
+                    Operation add = (x, y) => x + y;
+                    Console.WriteLine("x + y = " + add(x, y));
+                    break;
+                case 2:
+                    Operation sub = (x, y) => x - y;
+                    Console.WriteLine("x - y = " + sub(x, y));
+                    break;
+                case 3:
+                    Operation mul = (x, y) => x * y;
+                    Console.WriteLine("x * y = " + mul(x, y));
+                    break;
+                case 4:
+                    try
+                    {
+                        Operation div = (x, y) => x / y;
+                        Console.WriteLine("x / y = " + div(x, y));
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ glava12/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        public delegate string String(string s);
+        static string Insert(string s)
+        {
+            string s2 = s.Insert(8, " утром");
+            Console.WriteLine(s2);
+            return s2;
+        }
+        static string Replace (string s)
+        {
+            string s2 = s.Replace("и", "а");
+            Console.WriteLine(s2);
+            return s2;
+        }
+        static string Concat (string s)
+        {
+            string s2 = " утром ";
+            string str = s + s2;
+            Console.WriteLine(str);
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            string str = "Выйти на улицу";
+            String s = new String (Insert);
+            s += Replace;
+            s += Concat;
+            s(str);
+            Console.ReadLine();
+        }
+    }
+}
