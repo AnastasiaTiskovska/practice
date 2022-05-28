e2d7cea05a9ab41ec2bc9f0b906ab342122f0739
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-05-28 12:49:32 +0300

    glava14

---
+++ glava14/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace task2
+{
+    class Program
+    {
+        static void sum()
+        {
+            int summa = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                summa += i;
+            }
+            Console.WriteLine(summa);
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sWatch = new Stopwatch();
+            sWatch.Start();
+                Thread thread1 = new Thread(new ThreadStart(sum));
+                Thread thread2 = new Thread(new ThreadStart(sum));
+                thread1.Start();
+                thread2.Start();
+            sWatch.Stop();
+            Console.WriteLine($"Затраченное время в миллисекундах = {sWatch.ElapsedMilliseconds.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ glava14/task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Convert;
+using System.Diagnostics;
+
+namespace task3
+{
+    class Program
+    {
+        static void Sum(int a, int n)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(a, i);
+            }
+            Console.WriteLine("Сумма = " + sum);
+        }
+        static void Mul(int a, int n)
+        {
+            double mul = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                mul *= Math.Pow(a, i);
+            }
+            Console.WriteLine("Произведение = " + mul);
+        }
+        static void Main(string[] args)
+        {
+            Write("Введите А:");
+            int a = ToInt32(ReadLine());
+            Write("Введите N:");
+            int n = ToInt32(ReadLine());
+            Thread t1 = new Thread(delegate() { Sum(a, n); Mul(a, n); });
+            Thread t2 = new Thread(delegate () { Sum(a, n); });
+            t1.Start();
+            t2.Start();
+            Console.ReadKey();
+        }
+    }
+}
