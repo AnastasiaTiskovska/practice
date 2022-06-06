3afcb44896faf6fcea6cf873f13898980c7b2467
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-06-06 16:52:04 +0300

    glava21

---
+++ glava21/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Task1
+{
+    class Program
+    {
+        public class Singleton
+        {
+            private static object _sync = new object();
+
+            private static Singleton _instance;
+
+            private string _data;
+
+            public string Data
+            {
+                get
+                {
+                    return _data;
+                }
+                set
+                {
+                    lock (_sync)
+                    {
+                        _data = value;
+                    }
+                }
+            }
+
+            private Singleton(string data)
+            {
+                Data = data;
+            }
+
+            public static Singleton GetInstance(string data)
+            {
+                lock (_sync)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton(data);
+                    }
+                }
+                return _instance;
+            }
+
+            public override string ToString()
+            {
+                return Data;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.GetInstance("Ура, лето!");
+            var singleton2 = Singleton.GetInstance("Не хочу учиться!");
+            Console.WriteLine(singleton2.Data);
+            Console.ReadLine();
+        }
+    }
+}
