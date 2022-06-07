22124ab5fc043d2b2af9ff641c219a6fd1f25e0a
AnastasiaTiskovska <anastasiatiskovskaatiskovskaa@gmail.com>
2022-06-07 23:37:58 +0300

    glava22

---
+++ glava22/Task1/Задание1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Задание1 : Form
+    {
+        public Задание1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            textBox4.Text += "Глава №22, Тишковская А.С.";
+            textBox4.Text += Environment.NewLine + "X = " + x.ToString();
+            double y = double.Parse(textBox2.Text);
+            textBox4.Text += Environment.NewLine + "Y = " + y.ToString();
+            double z = double.Parse(textBox3.Text);
+            textBox4.Text += Environment.NewLine + "Z = " + z.ToString();
+            double a = Math.Tan(x + y) * Math.Tan(x + y);
+            double b = Math.Exp(y - z);
+            double c = Math.Sqrt(Math.Cos(x * x) + Math.Sin(z * z));
+            double u = a - b * c;
+            textBox4.Text += Environment.NewLine + "Результат U = " + u.ToString();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "3,4";
+            textBox2.Text = "0,74";
+            textBox3.Text = "19,43";
+        }
+    }
+}
---
+++ glava22/Task3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание начальных данных
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox5.Text);
+            textBox4.Text = "Работу выполнила ст. Тишковская А. С." +Environment.NewLine;
+            // Цикл для табулирования функции
+            double x = x0;
+            while (x <= (xk + dx / 2))
+            {
+                double y = a * Math.Log(x);
+                textBox4.Text += "x=" + Convert.ToString(x) + "; y=" + Convert.ToString(y) + Environment.NewLine;
+
+                x = x + dx;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
